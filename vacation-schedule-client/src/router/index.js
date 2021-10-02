90f50d8329defbcc9ebddddc74f18b26df72ac4f
Ivan Monastyrev <ikloster@yandex.ru>
2021-10-02 20:09:00 +0300

    client: update routes

--- vacation-schedule-client/src/router/index.js
+++ vacation-schedule-client/src/router/index.js
@@ -1,10 +1,34 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomePage from '../pages/HomePage.vue';
 import LoginPage from '../pages/LoginPage.vue';
+import { PAGES } from '../const';
 
 const routes = [
-  { path: '/', component: HomePage },
-  { path: '/login', component: LoginPage },
+  {
+    path: PAGES.HOME.PATH,
+    name: PAGES.HOME.NAME,
+    component: HomePage,
+    alias: PAGES.ALL_EVENTS.PATH,
+    meta: { requiresAuth: true },
+  },
+  {
+    path: PAGES.ALL_EVENTS.PATH,
+    name: PAGES.ALL_EVENTS.NAME,
+    component: HomePage,
+    meta: { requiresAuth: true },
+  },
+  {
+    path: PAGES.MY_EVENTS.PATH,
+    name: PAGES.MY_EVENTS.NAME,
+    component: HomePage,
+    meta: { requiresAuth: true },
+  },
+  {
+    path: PAGES.LOGIN.PATH,
+    name: PAGES.LOGIN.NAME,
+    component: LoginPage,
+    meta: { requiresAuth: false },
+  },
 ];
 
 const router = createRouter({
